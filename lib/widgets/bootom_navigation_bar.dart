ec4d1f210cd42d82a673f254e35af84551736933
kudrautsava.yuliya <kudrautsava.yuliya@gmail.com>
2022-09-02 11:53:54 +0300

    fix view BottomNavBar

--- lib/widgets/bootom_navigation_bar.dart
+++ lib/widgets/bootom_navigation_bar.dart
@@ -4,6 +4,7 @@ import 'package:mobile_phone_shop/pages/favorite_page.dart';
 import 'package:mobile_phone_shop/pages/profile_page.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mobile_phone_shop/themes/custom_color.dart';
+import 'package:mobile_phone_shop/themes/theme.dart';
 
 class CustomBottomNavigationBar extends StatefulWidget {
   const CustomBottomNavigationBar({Key? key}) : super(key: key);
@@ -29,69 +30,66 @@ class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-          child: widgetOptions.elementAt(selectedIndex),
-        ),
-        bottomNavigationBar: Container(
-          height: 72,
-          decoration: const BoxDecoration(
-            color: CustomColors.darkBlue,
-            borderRadius: BorderRadius.all(Radius.circular(30)),
-          ),
-          child: ClipRRect(
-              borderRadius: const BorderRadius.all(Radius.circular(30)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Row(
-                    children: const <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(right: 10),
-                        child: Icon(
-                          Icons.circle,
-                          color: Colors.white,
-                          size: 8,
-                        ),
-                      ),
-                      Text(
-                        'Explorer',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'MarkPro',
-                          fontWeight: FontWeight.w700,
-                          fontSize: 15,
-                        ),
-                      ),
-                    ],
+    return Container(
+      width: AppTheme.fullWidth(context),
+      height: 72,
+      decoration: const BoxDecoration(
+        color: CustomColors.darkBlue,
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+      ),
+      child: ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(30)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                children: const <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Icon(
+                      Icons.circle,
+                      color: Colors.white,
+                      size: 8,
+                    ),
                   ),
-                  IconButton(
-                    enableFeedback: false,
-                    onPressed: () {
-                      setState(() {
-                        selectedIndex = 0;
-                      });
-                    },
-                    icon: walletIcon,
+                  Text(
+                    'Explorer',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'MarkPro',
+                      fontWeight: FontWeight.w700,
+                      fontSize: 15,
+                    ),
                   ),
-                  IconButton(
-                      enableFeedback: false,
-                      onPressed: () {
-                        setState(() {
-                          selectedIndex = 1;
-                        });
-                      },
-                      icon: favoriteIcon),
-                  IconButton(
-                      enableFeedback: false,
-                      onPressed: () {
-                        setState(() {
-                          selectedIndex = 2;
-                        });
-                      },
-                      icon: profileIcon),
                 ],
-              )),
-        ));
+              ),
+              IconButton(
+                enableFeedback: false,
+                onPressed: () {
+                  setState(() {
+                    selectedIndex = 0;
+                  });
+                },
+                icon: walletIcon,
+              ),
+              IconButton(
+                  enableFeedback: false,
+                  onPressed: () {
+                    setState(() {
+                      selectedIndex = 1;
+                    });
+                  },
+                  icon: favoriteIcon),
+              IconButton(
+                  enableFeedback: false,
+                  onPressed: () {
+                    setState(() {
+                      selectedIndex = 2;
+                    });
+                  },
+                  icon: profileIcon),
+            ],
+          )),
+    );
   }
 }
